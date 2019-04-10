31c7c6d0b18476e1fa7d81073f42114bedc2ad84
Josephine Chong <josephine.yt.chong@gmail.com>
2019-04-10 16:09:58 +1000

    Completed code. Will run in irb or Ruby.

---
+++ Total_height.rb
@@ -0,0 +1,11 @@
+# Using IRB, and using google to get your info, assign the value of the eiffel tower, big ben and taj mahal to the three variables:
+# -> eiffel_tower (324m)
+# -> big_ben (96m)
+# -> taj_mahal (73m)
+# Using these variables,  calculate the height of all 3 landmarks added together, and store it in a variable called 'total_height'
+
+eiffel_tower = 324
+big_ben = 96
+taj_mahal = 73
+
+p total_height = eiffel_tower + big_ben + taj_mahal 
